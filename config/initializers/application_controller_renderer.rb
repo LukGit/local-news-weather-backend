3f44c0ace6ebaff6602ef14fb329d6ed364f2200
Shau Yin Luk <ivan.luk@icloud.com>
2021-02-09 15:20:21 -0600

    basic Rails setup

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,38 @@
-# Be sure to restart your server when you modify this file.
+class ApplicationController < ActionController::API
+  before_action :authorized
+  def encode_token(payload)
+    JWT.encode(payload, 'new$_weath#r', 'HS256')
+   end
 
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+  def auth_header
+    # { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # header: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, 'new$_weath#r', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+end
